e99471cea16cafe098760e999da194b3afa37233
Filipa <anafilipa.ferreira99@ua.pt>
2022-03-24 16:52:57 +0000

    Add message after register for persons with IDD

---
+++ HiLives/public/pt/components/messageRegister.php
@@ -0,0 +1,32 @@
+<div class="container">
+
+    <div class="row justify-content-center">
+
+        <div class="col-md-10 col-lg-6">
+
+            <div class="card o-hidden border-0 shadowCard my-5">
+                <div class="card-body p-0">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="p-5">
+                                <div class="text-center">
+                                    <a href="../../../index.php"><img class="pb-4 img-fluid reSize" src="../../img/logo.svg" alt="Logótipo do HiLives" title="Bem-vindo à HiLives!"></a>
+                                    <h1 class="mb-4 weightTitle">Espera que alguém entre em contacto contigo!</h1>
+                                    <p class="mb-4 descricao">Para teres acesso à Plataforma HiLives, precisas de esperar que alguém te contacte para uma entrevista. Entraremos em contacto contigo, no máximo, em duas semanas.</p>
+
+                                    <hr class="mt-4 mb-4">
+                                    <button class="btn buttonDesign buttonWork buttonRegisterSizeHEI m-0">
+                                        <a href="../../../index.php">Voltar à página inicial</a>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+
+</div>
---
+++ HiLives/public/pt/pages/messageRegister.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt">
+
+<head>
+    <?php include "../../helpers/meta.php"; ?>
+    <title>HiLives</title>
+    <?php include "../../helpers/fonts.php"; ?>
+    <?php include "../../helpers/css_forms.php"; ?>
+</head>
+
+<body class="bg_login_reg">
+    <?php include "../components/messageRegister.php"; ?>
+    <?php include "../../helpers/js.php"; ?>
+</body>
+
+</html>
