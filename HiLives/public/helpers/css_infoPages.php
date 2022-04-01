715011e89cf6bd2b3017be8fb58c60145d31f70a
Filipa <anafilipa.ferreira99@ua.pt>
2022-04-01 17:19:45 +0100

    Add accessibility page

---
+++ HiLives/public/helpers/css_infoPages.php
@@ -0,0 +1,3 @@
+<link href="../../css/infoPages.css" rel="stylesheet">
+<link href="../../css/nav.css" rel="stylesheet">
+<link href="../../css/index.css" rel="stylesheet">
---
+++ HiLives/public/pt/components/accessibility.php
@@ -0,0 +1,53 @@
+<div class="container pb-5">
+
+    <div class="mt-5">
+        <h1>Acessibilidade</h1>
+        <p>Esta página apresenta um conjunto de indicações sobre a melhor forma de navegar na plataforma HiLives. Esta foi desenvolvida para oferecer o máximo de acessibilidade aos utilizadores, seguindo as Diretrizes para Acessibilidade de Conteúdos Web – WCAG 2.1, desenvolvidas pelo W3C – World Wide Web Consortium.</p>
+        <p>As várias etapas de validação, que decorreram ao longo do processo de desenvolvimento, permitiram oferecer uma plataforma cujo acesso pode ser feito pelo teclado e leitor de ecrã com sintetizador de voz. Todas a acessibilidade da plataforma pode ser ativa através do icon de acessibilidade localizado no canto inferior direito.</p>
+    </div>
+
+    <div class="mt-5">
+        <h2>Descrição da plataforma</h2>
+        <p>A plataforma está organizada conforme descrito abaixo.</p>
+
+        <div class="mt-2">
+            <h3 class="subtitulo">Barra de Menu Geral</h3>
+            <ol class="infoSize">
+                <li>Logo da HiLives, com link para a Página Inicial</li>
+                <li>Links para aceder às principais páginas da Plataforma</li>
+                <li>Ícone da foto de perfil</li>
+                <li>Link para acesso à área pessoa</li>
+                <li>Submenu para escolher o idioma</li>
+            </ol>
+        </div>
+
+        <div class="mt-2">
+            <h3 class="subtitulo">Área de conteúdos</h3>
+            <p>Na homepage das pessoas é possível encontrar:</p>
+            <ol class="infoSize">
+                <li>Principais funcionalidades da mesma</li>
+                <li>Vídeo de como interagir com a plataforma</li>
+            </ol>
+            <p>Na homepage das Instituições de Ensino Superior é possível encontrar:</p>
+            <ol class="infoSize">
+                <li>Principais funcionalidades da mesma</li>
+                <li>Últimas entradas de pessoas</li>
+                <li>Últimas vagas publicadas por empresas</li>
+            </ol>
+            <p>Na homepage das empresas é possível encontrar:</p>
+            <ol class="infoSize">
+                <li>Principais funcionalidades da mesma</li>
+                <li>Ligações recentes</li>
+            </ol>
+        </div>
+
+        <div class="mt-2">
+            <h3 class="subtitulo">Rodapé</h3>
+            <ol class="infoSize">
+                <li>Lista de parceiros, com respetivo link individual</li>
+                <li>Atalhos para conteúdos importantes da aplicação</li>
+                <li>Outros links importantes</li>
+            </ol>
+        </div>
+    </div>
+</div>
