a730cb5ffe5fad74fd739f964e9cc2730ee32983
Filipa <anafilipa.ferreira99@ua.pt>
2022-04-02 14:23:43 +0100

    Add company homepage

---
+++ HiLives/public/pt/components/homeComp.php
@@ -0,0 +1,66 @@
+<!-- Header -->
+<div style="background: url(../../img/homePerson.png)" class="jumbotron bg-cover text-white">
+    <div class="container py-5 text-center">
+        <h1 class="fontWhite textBanner">Bem-vindo(a) ao HiLives!</h1>
+        <div class="arrow">
+            <a class="fa-solid fa-circle-chevron-down" href="#"></a>
+        </div>
+    </div>
+</div>
+
+<!-- Matchs -->
+<section class="conatiner-fluid greyBg">
+    <div class="container text-center pt-5 pb-5">
+        <h2 class="pb-4">Porquê a HiLives?</h2>
+        <div class="row">
+            <div class="col-12 col-md-6 ps-4 pe-4">
+                <img src="../../img/study.svg" alt="Ícone de um livro" class="img-fluid" title="Pessoa">
+                <h3 class="mt-4 pb-2">Ajudamo-lo a procurar candidatos</h3>
+                <p>Se está à procura de recursos humanos para a sua empresa, a HiLives vai indicar-lhe quais são os candidatos que melhor se enquadram nas características pretendidas.</p>
+                <br>
+                <a href="#">
+                    <button class="btn buttonDesign buttonWork buttonHomeCompSize m-0">
+                        Ver candidatos
+                    </button>
+                </a>
+            </div>
+            <div class="col-12 col-md-6 ps-4 pe-4">
+                <img src="../../img/work.svg" alt="Ícone de uma paste de trabalho" class="img-fluid" title="Instituição de Ensino Superior">
+                <h3 class="mt-4 pb-2">Ajudamo-lo a divulgar vagas de trabalho</h3>
+                <p>Se tem vagas disponíveis na sua empresa basta publicá-las nesta plataforma e a HiLives ajuda-o na sua divulgação junta das Pessoas com DID, basta publicar.</p>
+                <br>
+                <a href="#">
+                    <button class="btn buttonDesign buttonWork buttonHomeCompSize m-0">
+                        Adicionar vaga
+                    </button>
+                </a>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- HiLives Stories-->
+<section style="background: url(../../img/homeStories.png)" class="jumbotron bgCoverSection">
+    <div class="bg-white ps-3 pe-3">
+        <h3 class="pt-5 pb-5 text-center">Queres ver histórias das Pessoas com DID?</h3>
+        <ul>
+            <li class="pb-5">Tem acesso a histórias através de vídeos, textos, áudio e imagens.</li>
+            <li class="pb-5">Pode partilhar as histórias das pessoas com DID que trabalham na empresa.</li>
+        </ul>
+        <div class="text-center">
+            <a href="#">
+                <button class="btn buttonDesign buttonWork buttonHomeSize m-0">
+                    Ver Histórias da HiLives
+                </button>
+            </a>
+        </div>
+    </div>
+</section>
+<!-- HiLives video tutorial -->
+<section class="container-fluid greyBg">
+    <div class="container text-center pt-5 pb-5">
+        <h2 class="pb-3">Aprenda a utilizar a HiLives</h2>
+        <div class="video" controls>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/SZqio3YCUNU" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+    </div>
+</section>
---
+++ HiLives/public/pt/pages/homeComp.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt">
+
+<head>
+    <?php include "../../helpers/meta.php"; ?>
+    <title>HiLives</title>
+    <?php include "../../helpers/fonts.php"; ?>
+    <?php include "../../helpers/css_homePerson.php"; ?>
+</head>
+
+<body>
+    <?php include "../components/navbar.php"; ?>
+    <?php include "../components/homeComp.php"; ?>
+    <?php include "../components/footer.php"; ?>
+</body>
+
+</html>
