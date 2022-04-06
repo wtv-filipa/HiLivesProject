9483050a7435661ff07ff3e4854036c0946a0bfb
Filipa <anafilipa.ferreira99@ua.pt>
2022-04-06 12:26:12 +0100

    Add HiLives Stories page

---
+++ HiLives/public/pt/components/stories.php
@@ -0,0 +1,96 @@
+<nav class="navbar navbar-light navBgColor sticky-top">
+    <div class="container">
+        <span class="navbar-text navstories">
+            <a href="uploadStory.php">
+                Criar uma história
+                <i class="fa-solid fa-angle-right arrowRight"></i>
+            </a>
+        </span>
+    </div>
+</nav>
+
+<div class="container pb-5">
+    <nav style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='currentColor'/%3E%3C/svg%3E&#34;);" aria-label="breadcrumb" class="mt-4 col-md-6">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="homePerson.php">Página Inicial</a></li>
+            <li class="breadcrumb-item active" aria-current="page">Histórias do HiLives</li>
+        </ol>
+    </nav>
+
+    <h1 class="pb-2">Histórias do HiLives</h1>
+    <p>Aqui, vais encontrar vídeos, fotografias, áudios e textos que demonstram como foram as experiências académicas e profissionais de outros utilizadores da HiLives. Podes também encontrar vídeos que mostram os ambientes das empresas ou universidades. Qualquer pessoa pode publicar a sua história! Adiciona a tua aqui!</p>
+
+    <!--STORIES-->
+    <!--Video-->
+    <div class="wrapperStory">
+        <header class="cf">
+            <a href=#>
+                <img class="profile-pic" src="../../img/no_profile_img.png" alt="sem imagem de perfil" title="Imagem padrão" />
+            </a>
+            <h5 class="name">
+                <a href="#" class="linkStory">Filipa Ferreira</a>
+            </h5>
+            <p class="cardInfo13">A 15 de Fevereiro</p>
+        </header>
+        <p class="status">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eget nunc a ante pharetra placerat. Curabitur viverra neque sit amet interdum commodo.</p>
+        <div class="text-center">
+            <video width="600" controls>
+                <source src="mov_bbb.mp4" type="video/mp4">
+                <source src="mov_bbb.ogg" type="video/ogg">
+                Your browser does not support HTML video.
+            </video>
+        </div>
+    </div>
+
+    <!--Text-->
+    <div class="wrapperStory">
+        <header class="cf">
+            <a href=#>
+                <img class="profile-pic" src="../../img/no_profile_img.png" alt="sem imagem de perfil" title="Imagem padrão" />
+            </a>
+            <h5 class="name">
+                <a href="#" class="linkStory">Filipa Ferreira</a>
+            </h5>
+            <p class="cardInfo13">A 15 de Fevereiro</p>
+        </header>
+        <p class="status">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eget nunc a ante pharetra placerat. Curabitur viverra neque sit amet interdum commodo. Sed ultrices vitae nunc pharetra rutrum. In at dictum tortor. Nulla condimentum tellus laoreet elit fermentum, scelerisque tempus tortor congue. Integer varius vulputate volutpat. Cras non augue congue purus consectetur pellentesque sit amet ut leo. Donec lacinia id ante a congue. Aliquam erat volutpat. Nullam ante nibh, faucibus ac scelerisque id, rhoncus sit amet nunc. Maecenas mollis ipsum eu dolor sollicitudin aliquet nec non est. Mauris quis molestie lacus, ac ornare neque. Integer accumsan id nisl id tincidunt. Maecenas cursus purus dui, eu dignissim nisi faucibus eget.</p>
+    </div>
+
+    <!--Audio-->
+    <div class="wrapperStory">
+        <header class="cf">
+            <a href=#>
+                <img class="profile-pic" src="../../img/no_profile_img.png" alt="sem imagem de perfil" title="Imagem padrão" />
+            </a>
+            <h5 class="name">
+                <a href="#" class="linkStory">Filipa Ferreira</a>
+            </h5>
+            <p class="cardInfo13">A 15 de Fevereiro</p>
+        </header>
+        <p class="status">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eget nunc a ante pharetra placerat. Curabitur viverra neque sit amet interdum commodo.</p>
+        <div class="text-center">
+            <audio controls>
+                <source src="horse.ogg" type="audio/ogg">
+                <source src="horse.mp3" type="audio/mpeg">
+                Your browser does not support the audio element.
+            </audio>
+        </div>
+    </div>
+
+    <!--Image-->
+    <div class="wrapperStory">
+        <header class="cf">
+            <a href=#>
+                <img class="profile-pic" src="../../img/no_profile_img.png" alt="sem imagem de perfil" title="Imagem padrão" />
+            </a>
+            <h5 class="name">
+                <a href="#" class="linkStory">Filipa Ferreira</a>
+            </h5>
+            <p class="cardInfo13">A 15 de Fevereiro</p>
+        </header>
+        <p class="status">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eget nunc a ante pharetra placerat. Curabitur viverra neque sit amet interdum commodo.</p>
+        <div class="text-center">
+            <img class="img-content" src="https://www.tesla.com/sites/default/files/red-tesla-model-s.jpg" />
+        </div>
+    </div>
+</div>
---
+++ HiLives/public/pt/pages/stories.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt">
+
+<head>
+    <?php include "../../helpers/meta.php"; ?>
+    <title>HiLives</title>
+    <?php include "../../helpers/fonts.php"; ?>
+    <?php include "../../helpers/css_stories.php"; ?>
+</head>
+
+<body>
+    <?php include "../components/navbar.php"; ?>
+    <?php include "../components/stories.php"; ?>
+    <?php include "../components/footer.php"; ?>
+
+    <?php include "../../helpers/js.php"; ?>
+</body>
+
+</html>
