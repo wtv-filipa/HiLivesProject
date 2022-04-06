d9b46bddcb79eb7fb1db7a2ea9579c6949090a95
Filipa <anafilipa.ferreira99@ua.pt>
2022-04-06 18:19:01 +0100

    Add Tutor's edit requests

--- HiLives/public/pt/components/editRequestsTutor.php
+++ HiLives/public/pt/components/editRequestsTutor.php
@@ -2,24 +2,24 @@
     <nav style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='currentColor'/%3E%3C/svg%3E&#34;);" aria-label="breadcrumb" class="mt-4">
         <ol class="breadcrumb">
             <li class="breadcrumb-item"><a href="homePerson.php">Página Inicial</a></li>
-            <li class="breadcrumb-item active" aria-current="page">Pedidos de registo</li>
+            <li class="breadcrumb-item active" aria-current="page">Pedidos de edição</li>
         </ol>
     </nav>
 
-    <h1 class="pb-2">Pedidos de registo</h1>
-    <p class="pb-4">Nesta página é possível ver quais são os pedidos de registo que ainda estão pendentes.</p>
+    <h1 class="pb-2">Pedidos para editar o perfil</h1>
+    <p class="pb-4">Nesta página é possível ver quais são os pedidos para editar o perfil que ainda estão pendentes.</p>
 
     <section class="pb-5">
         <div class="row">
             <div class="col-md-4">
                 <div class="card text-center shadowCard o-hidden border-0">
                     <div class="card-body">
-                        <img class="imgProfilePerson borderPink mb-4" src="../../img/no_profile_img.png" alt="sem imagem de perfil" title="Imagem padrão" />
+                        <img class="imgProfilePerson borderBlue mb-4" src="../../img/no_profile_img.png" alt="sem imagem de perfil" title="Imagem padrão" />
                         <h4 class="pb-2">Mariana Silva</h4>
                         <p class="pb-0">marianamsilva19@gmail.com</p>
                         <p>Regions of interest: Aveiro, Braga</p>
                         <a href="#">
-                            <button class="btn buttonDesign buttonStudy buttonLoginSize m-0 mb-3">
+                            <button class="btn buttonDesign buttonWork buttonLoginSize m-0 mb-3">
                                 Ver pedido
                             </button>
                         </a>
@@ -30,12 +30,12 @@
             <div class="col-md-4">
                 <div class="card text-center shadowCard o-hidden border-0">
                     <div class="card-body">
-                        <img class="imgProfilePerson borderPink mb-4" src="../../img/no_profile_img.png" alt="sem imagem de perfil" title="Imagem padrão" />
+                        <img class="imgProfilePerson borderBlue mb-4" src="../../img/no_profile_img.png" alt="sem imagem de perfil" title="Imagem padrão" />
                         <h4 class="pb-2">Mariana Silva</h4>
                         <p class="pb-0">marianamsilva19@gmail.com</p>
                         <p>Regions of interest: Aveiro, Braga</p>
                         <a href="#">
-                            <button class="btn buttonDesign buttonStudy buttonLoginSize m-0 mb-3">
+                            <button class="btn buttonDesign buttonWork buttonLoginSize m-0 mb-3">
                                 Ver pedido
                             </button>
                         </a>
@@ -46,12 +46,12 @@
             <div class="col-md-4">
                 <div class="card text-center shadowCard o-hidden border-0">
                     <div class="card-body">
-                        <img class="imgProfilePerson borderPink mb-4" src="../../img/no_profile_img.png" alt="sem imagem de perfil" title="Imagem padrão" />
+                        <img class="imgProfilePerson borderBlue mb-4" src="../../img/no_profile_img.png" alt="sem imagem de perfil" title="Imagem padrão" />
                         <h4 class="pb-2">Mariana Silva</h4>
                         <p class="pb-0">marianamsilva19@gmail.com</p>
                         <p>Regions of interest: Aveiro, Braga</p>
                         <a href="#">
-                            <button class="btn buttonDesign buttonStudy buttonLoginSize m-0 mb-3">
+                            <button class="btn buttonDesign buttonWork buttonLoginSize m-0 mb-3">
                                 Ver pedido
                             </button>
                         </a>
