2ce631a71dda448dc2df336bc708a6baa0c3c496
Filipa <anafilipa.ferreira99@ua.pt>
2022-04-28 12:30:24 +0100

    Add be infos to editRequestsTutor and add message on registeRequestsTutor

--- HiLives/public/pt/components/editRequestsTutor.php
+++ HiLives/public/pt/components/editRequestsTutor.php
@@ -1,64 +1,123 @@
-<div class="container">
-    <nav style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='currentColor'/%3E%3C/svg%3E&#34;);" aria-label="breadcrumb" class="mt-4">
-        <ol class="breadcrumb">
-            <li class="breadcrumb-item"><a href="homeTutor.php" title="Voltar à página inicial">Página Inicial</a></li>
-            <li class="breadcrumb-item active" aria-current="page">Pedidos de edição</li>
-        </ol>
-    </nav>
+<?php
+require_once("../../connections/connection.php");
 
-    <h1 class="pb-2">Pedidos para editar o perfil</h1>
-    <p class="pb-4">Nesta página é possível ver quais são os pedidos para editar o perfil que ainda estão pendentes.</p>
+$link = new_db_connection();
+$stmt = mysqli_stmt_init($link);
 
-    <section class="pb-5">
-        <div class="row">
-            <div class="col-12 col-md-6 col-lg-4 mb-4">
-                <div class="card text-center shadowCard o-hidden border-0">
-                    <div class="card-body">
-                        <img class="imgProfilePerson borderBlue mb-4" src="../../img/no_profile_img.png" alt="sem imagem de perfil" title="Imagem padrão" />
-                        <h4 class="pb-2">Mariana Silva</h4>
-                        <p class="pb-0">marianamsilva19@gmail.com</p>
-                        <p>Regions of interest: Aveiro, Braga</p>
-                        <a href="IndividualReqEditTutor.php" title="Ver pedido de edição da">
-                            <button class="btn buttonDesign buttonWork buttonLoginSize m-0 mb-3">
-                                Ver pedido
-                            </button>
-                        </a>
-                    </div>
-                </div>
+$link2 = new_db_connection();
+$stmt2 = mysqli_stmt_init($link2);
+
+if ($_SESSION["idUser"]) {
+
+    $idUser = $_SESSION["idUser"];
+
+    $query = "SELECT idusers, name_user, email_user, profile_img
+    FROM users
+    WHERE user_type_iduser_type = 10 AND edit_request = 1
+    ORDER BY idusers DESC";
+
+    $query2 = "SELECT name_region
+    FROM region
+    INNER JOIN users_has_region ON region.idregion = users_has_region.region_idregion
+    INNER JOIN users ON users_has_region.users_idusers = users.idusers
+    WHERE users.user_type_iduser_type = 10 AND users.edit_request = 1 AND idusers = ?";
+?>
+    <div class="container">
+        <nav style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='currentColor'/%3E%3C/svg%3E&#34;);" aria-label="breadcrumb" class="mt-4">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="homeTutor.php" title="Voltar à página inicial">Página Inicial</a></li>
+                <li class="breadcrumb-item active" aria-current="page">Pedidos de edição</li>
+            </ol>
+        </nav>
+
+        <h1 class="pb-2">Pedidos para editar o perfil</h1>
+        <p class="pb-4">Nesta página é possível ver quais são os pedidos para editar o perfil que ainda estão pendentes.</p>
+
+        <section class="pb-5">
+            <div class="row">
+                <?php
+
+                if (mysqli_stmt_prepare($stmt, $query)) {
+                    mysqli_stmt_execute($stmt);
+                    mysqli_stmt_bind_result($stmt, $idusers, $name_user, $email_user, $profile_img);
+                    mysqli_stmt_store_result($stmt);
+                    if (mysqli_stmt_num_rows($stmt) > 0) {
+                        while (mysqli_stmt_fetch($stmt)) {
+                ?>
+                            <div class="col-12 col-md-6 col-lg-4 mb-4">
+                                <div class="card text-center shadowCard o-hidden border-0">
+                                    <div class="card-body">
+                                        <?php
+                                        if (isset($profile_img)) {
+                                        ?>
+                                            <img class="imgProfilePerson borderBlue mb-4" src="../../../admin/uploads/img_perfil/<?= $profile_img ?>" alt="<?= $profile_img ?>" title="Imagem de perfil de <?= $name_user ?>" />
+                                        <?php
+                                        } else {
+                                        ?>
+                                            <img class="imgProfilePerson borderBlue mb-4" src="../../img/no_profile_img.png" alt="sem imagem de perfil" title="Imagem padrão" />
+                                        <?php
+                                        }
+                                        ?>
+                                        <h4 class="pb-2"><?= $name_user ?></h4>
+                                        <p class="pb-0"><?= $email_user ?></p>
+                                        <p>
+                                            Regiões de interesse:
+                                            <?php
+                                            $first = true;
+                                            if (mysqli_stmt_prepare($stmt2, $query2)) {
+                                                mysqli_stmt_bind_param($stmt2, 'i', $idusers);
+                                                mysqli_stmt_execute($stmt2);
+                                                mysqli_stmt_bind_result($stmt2, $name_region);
+                                                while (mysqli_stmt_fetch($stmt2)) {
+                                                    if (!$first) {
+                                                        echo ",";
+                                                    }
+                                                    $first = false;
+                                                    echo " $name_region";
+                                                }
+                                            }
+                                            ?>
+                                        </p>
+                                        <a href="IndividualReqEditTutor.php?edit=<?= $idusers ?>" title="Ver pedido de edição da">
+                                            <button class="btn buttonDesign buttonWork buttonLoginSize m-0 mb-3">
+                                                Ver pedido
+                                            </button>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        <?php
+                        }
+                    } else {
+                        ?>
+                        <section class="row justify-content-center">
+                            <div class="col-12 col-md-6 col-lg-4 mb-4">
+                                <div class="card text-center shadowCard o-hidden border-0">
+                                    <div class="card-body  pt-5 pb-5">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" fill="currentColor" class="bi bi-x-circle mb-3" viewBox="0 0 16 16">
+                                            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                                            <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
+                                        </svg>
+                                        <p class="mx-auto" style="font-size: 1rem;">
+                                            Neste momento não existe nenhum pedido de edição pendente. Por favor volte mais tarde.
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
+                <?php
+                    }
+                }
+                ?>
             </div>
+        </section>
 
-            <div class="col-12 col-md-6 col-lg-4 mb-4">
-                <div class="card text-center shadowCard o-hidden border-0">
-                    <div class="card-body">
-                        <img class="imgProfilePerson borderBlue mb-4" src="../../img/no_profile_img.png" alt="sem imagem de perfil" title="Imagem padrão" />
-                        <h4 class="pb-2">Mariana Silva</h4>
-                        <p class="pb-0">marianamsilva19@gmail.com</p>
-                        <p>Regions of interest: Aveiro, Braga</p>
-                        <a href="IndividualReqEditTutor.php" title="Ver pedido de edição da">
-                            <button class="btn buttonDesign buttonWork buttonLoginSize m-0 mb-3">
-                                Ver pedido
-                            </button>
-                        </a>
-                    </div>
-                </div>
-            </div>
+    </div>
+<?php
+} else {
+    include("404.php");
+}
 
-            <div class="col-12 col-md-6 col-lg-4 mb-4">
-                <div class="card text-center shadowCard o-hidden border-0">
-                    <div class="card-body">
-                        <img class="imgProfilePerson borderBlue mb-4" src="../../img/no_profile_img.png" alt="sem imagem de perfil" title="Imagem padrão" />
-                        <h4 class="pb-2">Mariana Silva</h4>
-                        <p class="pb-0">marianamsilva19@gmail.com</p>
-                        <p>Regions of interest: Aveiro, Braga</p>
-                        <a href="IndividualReqEditTutor.php" title="Ver pedido de edição da">
-                            <button class="btn buttonDesign buttonWork buttonLoginSize m-0 mb-3">
-                                Ver pedido
-                            </button>
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
+mysqli_close($link);
 
-</div>
+?>
