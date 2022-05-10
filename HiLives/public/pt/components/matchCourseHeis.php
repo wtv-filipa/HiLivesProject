553bec1fc3d1a4dd43ee0897a36a8ecbe10a0dc7
Filipa <anafilipa.ferreira99@ua.pt>
2022-05-10 23:34:57 +0100

    Add be info to match courses Heis

--- HiLives/public/pt/components/matchCourseHeis.php
+++ HiLives/public/pt/components/matchCourseHeis.php
@@ -1,93 +1,90 @@
-<div class="container">
-    <nav style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='currentColor'/%3E%3C/svg%3E&#34;);" aria-label="breadcrumb" class="mt-4">
-        <ol class="breadcrumb">
-            <li class="breadcrumb-item"><a href="homePerson.php" title="Voltar à página inicial">Página Inicial</a></li>
-            <li class="breadcrumb-item active" aria-current="page">Candidatos</li>
-        </ol>
-    </nav>
+<?php
+require_once("../../connections/connection.php");
 
-    <h1 class="pb-2">Candidatos | Cursos nas Instituições de Ensino Superior</h1>
-    <p class="pb-4">Nesta página encontra todas as suas ligações com possíveis candidatos a Cursos nas Instituições de Ensino Superior.</p>
+$link = new_db_connection();
+$stmt = mysqli_stmt_init($link);
 
-    <section class="row pb-5">
-        <div id="cardMatch" class="col-12 col-md-6 col-lg-4 pb-4">
-            <div class="list listStudy text-center">
-                <p>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-book align-middle" viewBox="0 0 16 16">
-                        <path d="M1 2.828c.885-.37 2.154-.769 3.388-.893 1.33-.134 2.458.063 3.112.752v9.746c-.935-.53-2.12-.603-3.213-.493-1.18.12-2.37.461-3.287.811V2.828zm7.5-.141c.654-.689 1.782-.886 3.112-.752 1.234.124 2.503.523 3.388.893v9.923c-.918-.35-2.107-.692-3.287-.81-1.094-.111-2.278-.039-3.213.492V2.687zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783z" />
-                    </svg>
-                    <span class="ps-2 align-middle">Estudar</span>
-                </p>
-                <h4>Filipa Ferreira</h4>
-                <p>Novas Tecnologias da Comunicação</p>
-            </div>
-        </div>
+if ($_SESSION["idUser"]) {
 
-        <div id="cardMatch" class="col-12 col-md-6 col-lg-4 pb-4">
-            <div class="list listStudy text-center">
-                <p>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-book align-middle" viewBox="0 0 16 16">
-                        <path d="M1 2.828c.885-.37 2.154-.769 3.388-.893 1.33-.134 2.458.063 3.112.752v9.746c-.935-.53-2.12-.603-3.213-.493-1.18.12-2.37.461-3.287.811V2.828zm7.5-.141c.654-.689 1.782-.886 3.112-.752 1.234.124 2.503.523 3.388.893v9.923c-.918-.35-2.107-.692-3.287-.81-1.094-.111-2.278-.039-3.213.492V2.687zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783z" />
-                    </svg>
-                    <span class="ps-2 align-middle">Estudar</span>
-                </p>
-                <h4>Filipa Ferreira</h4>
-                <p>Novas Tecnologias da Comunicação</p>
-            </div>
-        </div>
+    $idUser = $_SESSION["idUser"];
 
-        <div id="cardMatch" class="col-12 col-md-6 col-lg-4 pb-4">
-            <div class="list listStudy text-center">
-                <p>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-book align-middle" viewBox="0 0 16 16">
-                        <path d="M1 2.828c.885-.37 2.154-.769 3.388-.893 1.33-.134 2.458.063 3.112.752v9.746c-.935-.53-2.12-.603-3.213-.493-1.18.12-2.37.461-3.287.811V2.828zm7.5-.141c.654-.689 1.782-.886 3.112-.752 1.234.124 2.503.523 3.388.893v9.923c-.918-.35-2.107-.692-3.287-.81-1.094-.111-2.278-.039-3.213.492V2.687zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783z" />
-                    </svg>
-                    <span class="ps-2 align-middle">Estudar</span>
-                </p>
-                <h4>Filipa Ferreira</h4>
-                <p>Novas Tecnologias da Comunicação</p>
-            </div>
-        </div>
+    $query1 = "SELECT users_has_courses.users_idusers, users_has_courses.courses_idcourses, courses.idcourses, courses.name_course, courses.users_idusers, users.name_user
+    FROM users_has_courses
+    INNER JOIN courses ON users_has_courses.courses_idcourses = courses.idcourses
+    INNER JOIN users ON users.idusers = users_has_courses.users_idusers
+    WHERE courses.users_idusers = ?
+    ORDER BY id_match_course DESC";
+?>
+    <div class="container">
+        <nav style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='currentColor'/%3E%3C/svg%3E&#34;);" aria-label="breadcrumb" class="mt-4">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="homePerson.php" title="Voltar à página inicial">Página Inicial</a></li>
+                <li class="breadcrumb-item active" aria-current="page">Candidatos</li>
+            </ol>
+        </nav>
 
-        <div id="cardMatch" class="col-12 col-md-6 col-lg-4 pb-4">
-            <div class="list listStudy text-center">
-                <p>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-book align-middle" viewBox="0 0 16 16">
-                        <path d="M1 2.828c.885-.37 2.154-.769 3.388-.893 1.33-.134 2.458.063 3.112.752v9.746c-.935-.53-2.12-.603-3.213-.493-1.18.12-2.37.461-3.287.811V2.828zm7.5-.141c.654-.689 1.782-.886 3.112-.752 1.234.124 2.503.523 3.388.893v9.923c-.918-.35-2.107-.692-3.287-.81-1.094-.111-2.278-.039-3.213.492V2.687zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783z" />
-                    </svg>
-                    <span class="ps-2 align-middle">Estudar</span>
-                </p>
-                <h4>Filipa Ferreira</h4>
-                <p>Novas Tecnologias da Comunicação</p>
-            </div>
-        </div>
+        <h1 class="pb-2">Candidatos | Cursos nas Instituições de Ensino Superior</h1>
+        <p class="pb-4">Nesta página encontra todas as suas ligações com possíveis candidatos a Cursos nas Instituições de Ensino Superior.</p>
 
-        <div id="cardMatch" class="col-12 col-md-6 col-lg-4 pb-4">
-            <div class="list listStudy text-center">
-                <p>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-book align-middle" viewBox="0 0 16 16">
-                        <path d="M1 2.828c.885-.37 2.154-.769 3.388-.893 1.33-.134 2.458.063 3.112.752v9.746c-.935-.53-2.12-.603-3.213-.493-1.18.12-2.37.461-3.287.811V2.828zm7.5-.141c.654-.689 1.782-.886 3.112-.752 1.234.124 2.503.523 3.388.893v9.923c-.918-.35-2.107-.692-3.287-.81-1.094-.111-2.278-.039-3.213.492V2.687zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783z" />
-                    </svg>
-                    <span class="ps-2 align-middle">Estudar</span>
-                </p>
-                <h4>Filipa Ferreira</h4>
-                <p>Novas Tecnologias da Comunicação</p>
-            </div>
-        </div>
+        <section class="row pb-5">
+            <?php
 
-        <div id="cardMatch" class="col-12 col-md-6 col-lg-4 pb-4">
-            <div class="list listStudy text-center">
-                <p>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-book align-middle" viewBox="0 0 16 16">
-                        <path d="M1 2.828c.885-.37 2.154-.769 3.388-.893 1.33-.134 2.458.063 3.112.752v9.746c-.935-.53-2.12-.603-3.213-.493-1.18.12-2.37.461-3.287.811V2.828zm7.5-.141c.654-.689 1.782-.886 3.112-.752 1.234.124 2.503.523 3.388.893v9.923c-.918-.35-2.107-.692-3.287-.81-1.094-.111-2.278-.039-3.213.492V2.687zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783z" />
-                    </svg>
-                    <span class="ps-2 align-middle">Estudar</span>
-                </p>
-                <h4>Filipa Ferreira</h4>
-                <p>Novas Tecnologias da Comunicação</p>
-            </div>
-        </div>
+            if (mysqli_stmt_prepare($stmt, $query1)) {
 
-    </section>
+                mysqli_stmt_bind_param($stmt, 'i', $idUser);
+                mysqli_stmt_execute($stmt);
+                mysqli_stmt_bind_result($stmt, $users_idusers, $courses_idcourses, $idcourses, $name_course, $users_idusers, $name_user);
+                mysqli_stmt_store_result($stmt);
+                if (mysqli_stmt_num_rows($stmt) > 0) {
+                    while (mysqli_stmt_fetch($stmt)) {
+            ?>
+                        <div id="cardMatch" class="col-12 col-md-6 col-lg-4 pb-4">
+                            <div class="list listStudy text-center">
+                                <p>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-book align-middle" viewBox="0 0 16 16">
+                                        <path d="M1 2.828c.885-.37 2.154-.769 3.388-.893 1.33-.134 2.458.063 3.112.752v9.746c-.935-.53-2.12-.603-3.213-.493-1.18.12-2.37.461-3.287.811V2.828zm7.5-.141c.654-.689 1.782-.886 3.112-.752 1.234.124 2.503.523 3.388.893v9.923c-.918-.35-2.107-.692-3.287-.81-1.094-.111-2.278-.039-3.213.492V2.687zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783z" />
+                                    </svg>
+                                    <span class="ps-2 align-middle">Estudar</span>
+                                </p>
+                                <a href="profile.php?user=<?= $users_idusers ?>" title="Ver área de <?= $name_user ?>">
+                                    <h4><?= $name_user ?></h4>
+                                </a>
+                                <a href="infoCourse.php?course=<?= $courses_idcourses ?>" title="Ver informação da vaga <?= $vacancy_name ?>">
+                                    <p><?= $name_course ?></p>
+                                </a>
+                            </div>
+                        </div>
+                    <?php
+                    }
+                } else {
+                    ?>
+                    <section class="row justify-content-center">
+                        <div class="col-12 col-md-6 col-lg-4 mb-4">
+                            <div class="card text-center shadowCard o-hidden border-0">
+                                <div class="card-body  pt-5 pb-5">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" fill="currentColor" class="bi bi-x-circle mb-3" viewBox="0 0 16 16">
+                                        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                                        <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
+                                    </svg>
+                                    <p class="mx-auto" style="font-size: 1rem;">
+                                        Ainda não tem nenhuma ligação com os seus cursos. Por favor volte mais tarde.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+            <?php
+                }
+            }
+            ?>
+        </section>
 
-</div>
+    </div>
+<?php
+} else {
+    include("404.php");
+}
+
+mysqli_close($link);
+
+?>
