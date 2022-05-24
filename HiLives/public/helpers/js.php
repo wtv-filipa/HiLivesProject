abd4a54b28e0c7a4954698c89d27c8144226edfc
Filipa <anafilipa.ferreira99@ua.pt>
2022-05-24 22:32:50 +0100

    Edit userWay script

--- HiLives/public/helpers/js.php
+++ HiLives/public/helpers/js.php
@@ -5,7 +5,7 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
 <!--userway-->
-<script data-account="9sijSpsZ5M" src="https://cdn.userway.org/widget.js"></script>
+<script>(function(d){var s = d.createElement("script");s.setAttribute("data-account", "9sijSpsZ5M");s.setAttribute("src", "https://cdn.userway.org/widget.js");(d.body || d.head).appendChild(s);})(document)</script><noscript>Please ensure Javascript is enabled for purposes of <a href="https://userway.org">website accessibility</a></noscript>
 
 <!--fontawesome-->
 <script src="https://kit.fontawesome.com/55f787b303.js" crossorigin="anonymous"></script>
