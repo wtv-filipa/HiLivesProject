027544c608298f832298a1f8876a201d21e61396
Filipa <anafilipa.ferreira99@ua.pt>
2022-05-26 14:36:28 +0100

    Add files to start toltips and popovers in edit vacancy

--- HiLives/public/pt/pages/editVacancy.php
+++ HiLives/public/pt/pages/editVacancy.php
@@ -21,6 +21,8 @@ if (isset($_SESSION["idUser"]) && $_SESSION["type"] == 7) {
         <?php include "../../helpers/js_characterCounter.php"; ?>
         <?php include "../../helpers/js_upload.php"; ?>
         <?php include "../../helpers/formsValidation.php"; ?>
+        <?php include "../../helpers/js_enablePopover.php"; ?>
+        <?php include "../../helpers/js_enableToltip.php"; ?>
     </body>
 
     </html>
