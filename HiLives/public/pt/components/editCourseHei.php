1018005108b17119935e837d3a9ca4eeb4043d56
Filipa <anafilipa.ferreira99@ua.pt>
2022-05-26 14:35:05 +0100

    Add name course

--- HiLives/public/pt/components/editCourseHei.php
+++ HiLives/public/pt/components/editCourseHei.php
@@ -39,7 +39,7 @@ if (isset($_SESSION["idUser"]) && isset($_GET["course"])) {
                     <ol class="breadcrumb">
                         <li class="breadcrumb-item"><a href="homeHei.php" title="Voltar à página inicial">Página Inicial</a></li>
                         <li class="breadcrumb-item"><a href="allCoursesHeis.php" title="Voltar aos meus cursos">Cursos</a></li>
-                        <li class="breadcrumb-item active" aria-current="page">Editar um curso</li>
+                        <li class="breadcrumb-item active" aria-current="page">Editar o curso <?= $name_course ?></li>
                     </ol>
                 </nav>
 
@@ -87,7 +87,7 @@ if (isset($_SESSION["idUser"]) && isset($_GET["course"])) {
                                 <div class="paddingForms">
                                     <div class="text-center">
                                         <h1 class="mb-4 weightTitle">
-                                            Editar o curso
+                                            Editar o curso <?= $name_course ?>
                                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-question-circle" viewBox="0 0 16 16" tabindex="0" data-bs-toggle="popover" data-bs-trigger="focus" title="Dicas" data-bs-content="Utilize uma linguagem simples e frases curtas. Sempre que encontrar um símbolo semelhante junto dos campos a preencher, poderá encontrar dicas de como preencher os mesmos. Caso pretenda que o texto apareça com parágrafos, coloque cada parágrafo entre '<p></p>'. Se pretender destacar alguma palavra coloque-a entre '<b></b>'">
                                                 <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
                                                 <path d="M5.255 5.786a.237.237 0 0 0 .241.247h.825c.138 0 .248-.113.266-.25.09-.656.54-1.134 1.342-1.134.686 0 1.314.343 1.314 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.003.217a.25.25 0 0 0 .25.246h.811a.25.25 0 0 0 .25-.25v-.105c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.267 0-2.655.59-2.75 2.286zm1.557 5.763c0 .533.425.927 1.01.927.609 0 1.028-.394 1.028-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94z" />
