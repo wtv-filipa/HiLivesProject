c045b6769e28c4aa9d549a4a404778a7a141e9bb
Filipa <anafilipa.ferreira99@ua.pt>
2022-06-02 22:31:41 +0100

    Hide add storie button to tutor in pt version

--- HiLives/public/pt/components/stories.php
+++ HiLives/public/pt/components/stories.php
@@ -14,16 +14,22 @@ if (isset($_SESSION["idUser"]) && isset($_SESSION["type"])) {
     INNER JOIN users ON experiences.users_idusers = users.idusers
     ORDER BY idexperiences DESC";
 ?>
-    <nav class="navbar navbar-light navBgColor sticky-top">
-        <div class="container">
-            <span class="navbar-text navstories">
-                <a href="uploadStory.php" title="Adicionar uma nova história">
-                    Criar uma história
-                    <i class="fa-solid fa-angle-right arrowRight"></i>
-                </a>
-            </span>
-        </div>
-    </nav>
+    <?php
+    if ($User_type != 16) {
+    ?>
+        <nav class="navbar navbar-light navBgColor sticky-top">
+            <div class="container">
+                <span class="navbar-text navstories">
+                    <a href="uploadStory.php" title="Adicionar uma nova história">
+                        Criar uma história
+                        <i class="fa-solid fa-angle-right arrowRight"></i>
+                    </a>
+                </span>
+            </div>
+        </nav>
+    <?php
+    }
+    ?>
 
     <div class="container pb-5">
         <nav style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='currentColor'/%3E%3C/svg%3E&#34;);" aria-label="breadcrumb" class="mt-4 col-md-6">
