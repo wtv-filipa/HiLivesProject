f965dd854d1a760c9621d1ed22692a14b7dda091
Filipa <anafilipa.ferreira99@ua.pt>
2022-06-19 23:37:44 +0100

    Add info when HEI doesn't have a course published

--- HiLives/public/is/components/profileHei.php
+++ HiLives/public/is/components/profileHei.php
@@ -201,30 +201,51 @@ if (isset($_GET["user"]) && $_SESSION["idUser"]) {
                                     mysqli_stmt_bind_param($stmt2, 'i', $idUser);
                                     mysqli_stmt_execute($stmt2);
                                     mysqli_stmt_bind_result($stmt2, $idcourses, $name_course, $course_director);
-                                    while (mysqli_stmt_fetch($stmt2)) {
+                                    mysqli_stmt_store_result($stmt2);
+                                    if (mysqli_stmt_num_rows($stmt2) > 0) {
+                                        while (mysqli_stmt_fetch($stmt2)) {
                                 ?>
-                                        <div id="cardInfo" class="col-12 col-md-6 col-lg-4 pb-3">
-                                            <div class="items itemsStudy itemsBigger">
-                                                <h5><?= $name_course ?></h5>
-                                                <p class="cardInfo14 mb-2">Leikstjóri: <?= $course_director ?></p>
-                                                <div>
-                                                    <a href="editCourseHei.php?course=<?= $idcourses ?>" class="linkIconsStudy" title="Breyta námskeiðinu <?= $course_director ?>">
-                                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square me-3" viewBox="0 0 16 16">
-                                                            <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
-                                                            <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
-                                                        </svg>
-                                                    </a>
+                                            <div id="cardInfo" class="col-12 col-md-6 col-lg-4 pb-3">
+                                                <div class="items itemsStudy itemsBigger">
+                                                    <h5><?= $name_course ?></h5>
+                                                    <p class="cardInfo14 mb-2">Leikstjóri: <?= $course_director ?></p>
+                                                    <div>
+                                                        <a href="editCourseHei.php?course=<?= $idcourses ?>" class="linkIconsStudy" title="Breyta námskeiðinu <?= $course_director ?>">
+                                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square me-3" viewBox="0 0 16 16">
+                                                                <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                                                                <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
+                                                            </svg>
+                                                        </a>
 
-                                                    <a href="#" data-bs-toggle="modal" data-bs-target="#deleteCourseHei<?= $idcourses ?>" class="linkIconsStudy" title="Eyða námskeiðinu <?= $course_director ?>">
-                                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
-                                                            <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z" />
-                                                        </svg>
-                                                    </a>
+                                                        <a href="#" data-bs-toggle="modal" data-bs-target="#deleteCourseHei<?= $idcourses ?>" class="linkIconsStudy" title="Eyða námskeiðinu <?= $course_director ?>">
+                                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
+                                                                <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z" />
+                                                            </svg>
+                                                        </a>
+                                                    </div>
                                                 </div>
                                             </div>
-                                        </div>
+                                        <?php
+                                            include('../components/deleteModal.php');
+                                        }
+                                    } else {
+                                        ?>
+                                        <section class="row justify-content-center">
+                                            <div class="col-12 col-md-6 col-lg-4 mb-4">
+                                                <div class="card text-center shadowCard o-hidden border-0">
+                                                    <div class="card-body  pt-5 pb-5">
+                                                        <svg xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" fill="currentColor" class="bi bi-x-circle mb-3" viewBox="0 0 16 16">
+                                                            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                                                            <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
+                                                        </svg>
+                                                        <p class="mx-auto" style="font-size: 1rem;">
+                                                            Á þessum tíma hefur ekkert námskeið verið bætt við.
+                                                        </p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </section>
                                 <?php
-                                        include('../components/deleteModal.php');
                                     }
                                     mysqli_stmt_close($stmt2);
                                 }
--- HiLives/public/pt/components/allCoursesHeis.php
+++ HiLives/public/pt/components/allCoursesHeis.php
@@ -104,9 +104,27 @@ if (isset($_SESSION["idUser"])) {
                                 </div>
                             </div>
                         </div>
-            <?php
+                    <?php
                         include('../components/deleteModal.php');
                     }
+                } else {
+                    ?>
+                    <section class="row justify-content-center">
+                        <div class="col-12 col-md-6 col-lg-4 mb-4">
+                            <div class="card text-center shadowCard o-hidden border-0">
+                                <div class="card-body  pt-5 pb-5">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" fill="currentColor" class="bi bi-x-circle mb-3" viewBox="0 0 16 16">
+                                        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                                        <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
+                                    </svg>
+                                    <p class="mx-auto" style="font-size: 1rem;">
+                                        Neste momento não tem nenhum curso adicionado.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+            <?php
                 }
             }
             ?>
